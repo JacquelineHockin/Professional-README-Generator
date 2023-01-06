757f7dcf40110ec4f893fd2bfe5f5746dc21b731
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-01-05 17:45:50 -1000

    added to index.js

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown"); //app specific user package
 
-// TODO: Create an array of questions for user input
+// array of questions for user input
 const questions = [
   {
     type: "input",
     name: "title",
-    message: "What is the title of the project?",
+    message: "What is your project title?",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "oop. enter value to continue";
+      }
+    },
   },
   {
     type: "input",
     name: "description",
-    message: "What is the description of the project?",
+    message: "What is a brief description of your project?",
+    validate: (value) => {
+      if (value) {
+        return true;
+      } else {
+        return "oop. enter a value to continue";
+      }
+    },
   },
   {
     type: "input",
@@ -63,43 +77,25 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//   return fs.writeFileSync(path.join("__dirname", "fileName"), data);
+// }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  //call the inquierer prompt to get to know about the user's project
+  inquirer.prompt(questions).then((answers) => {
+    console.log("answers = ", answers);
+    //use the user's answers to create readme template string by calling generateMarkDown
+    const markDownContent = generateMarkdown(answers);
+    //write the readme template string into a file
+    fs.writeFile("generatedReadMe.md", markDownContent, (err) =>
+      err
+        ? console.log(err)
+        : console.log("Successfully created generatedReadMe.md!")
+    );
+  });
+}
 
 // Function call to initialize app
 init();
-
-///---
-const generateHTML = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
-
-inquirer.prompt().then((answers) => {
-  const htmlPageContent = generateHTML(answers);
-
-  fs.writeFile("index.html", htmlPageContent, (err) =>
-    err ? console.log(err) : console.log("Successfully created index.html!")
-  );
-});
