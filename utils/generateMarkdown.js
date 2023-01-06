b4641d26232cde703bc5761648ffd2a26ebe5566
JacquelineHockin <jacquelinehcashman@gmail.com>
2023-01-05 18:59:07 -1000

    added function that returns the license section of README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -50,9 +50,17 @@ function renderLicenseLink(license) {
   return licenseLink;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function that returns the license section of README
+// If there is no license, returns an empty string
+function renderLicenseSection(license) {
+  let licenseSection = "";
+  if (license === "None of the Above") {
+    licenseSection = "";
+  } else {
+    licenseSection = `License: ${license}`;
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
